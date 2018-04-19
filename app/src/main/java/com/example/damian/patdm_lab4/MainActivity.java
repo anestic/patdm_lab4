bcb2e09698a7adf5a01eacc299b3bced6dff72d0
anestic <szelagowskidamian@gmail.com>
2018-04-19 10:44:27 +0200

    MainActivity

--- app/src/main/java/com/example/damian/patdm_lab4/MainActivity.java
+++ app/src/main/java/com/example/damian/patdm_lab4/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.damian.patdm_lab4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Hello GitHub", Toast.LENGTH_SHORT).show();
+        String[] values = new String[] { "Red", "Green", "Blue", "Yellow", "Black", "White" };
+
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this, DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
 }
